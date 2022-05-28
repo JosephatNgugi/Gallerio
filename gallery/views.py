9a1d3ccc55988c9b05966f3f8a2ffc46a5976a5b
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-28 15:30:33 +0300

    create a routing url to the homepage

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,7 @@
+import datetime as dt
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    date = dt.date.today()
+    return render(request, 'galleries/index.html', {"date":date})
