5c43c895258506b40e170d0dbcd5c15713e3f179
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-28 16:20:48 +0300

    add a user class model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# User Model
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    
+    def __str__(self):
+        return self.first_name
+# Image Model
+# Location Model
+# Categories Model
