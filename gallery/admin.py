5c5ea2388e0280f4dec943fb4a394539b44ed520
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-28 18:31:11 +0300

    migrate models and register them to the admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    
+admin.site.register(User)
+admin.site.register(Image, ImageAdmin)
