feccaa5bd8efbf0ba1d6ab4782c8ec3306bbf6fc
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-29 16:26:41 +0300

    define methods that will save, delete and update category

--- gallery/models.py
+++ gallery/models.py
@@ -82,6 +82,17 @@ class Location(models.Model):
 # Categories Model
 class Category(models.Model):
     name = models.CharField(max_length=30)
+    
+    def save_category(self):
+        self.save()
+    
+    def delete_category(self):
+        self.delete()
+
+    @classmethod
+    def update_category(cls,id,name):
+        cls.objects.filter(id=id).update(name=name)
+
 
     def __str__(self):
         return self.name
