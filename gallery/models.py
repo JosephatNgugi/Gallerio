397f8080680206ec70a16f348ad94a8549116081
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-29 16:23:50 +0300

    define methods to save, delete and update location

--- gallery/models.py
+++ gallery/models.py
@@ -67,6 +67,16 @@ class Image(models.Model):
 class Location(models.Model):
     name = models.CharField(max_length=30)
     
+    def save_location(self):
+        self.save()
+        
+    def delete_location(self):
+        self.delete()
+       
+    @classmethod
+    def update_location(cls,id,name):
+        cls.objects.filter(id=id).update(name=name)
+         
     def __str__(self):
         return self.name
 # Categories Model
