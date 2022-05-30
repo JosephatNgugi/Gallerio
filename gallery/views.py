ce29f0ca325510b74d6a05dc0fc3203c4b02fe96
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-30 13:38:36 +0300

    define a view function to diplay images on the Homepage

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,10 @@
 import datetime as dt
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def homepage(request):
     date = dt.date.today()
-    return render(request, 'galleries/index.html', {"date":date})
+    images = Image.objects.all()
+    return render(request, 'galleries/index.html', {"date":date, "images":images})
+
