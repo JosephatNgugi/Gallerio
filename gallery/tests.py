1d6fca7d63e333e6976723a8b800ef1cd74ecfdf
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-31 06:48:51 +0300

    create tests category model

--- gallery/tests.py
+++ gallery/tests.py
@@ -83,3 +83,21 @@ class TestLocationModel(TestCase):
         location = Location.objects.all()
         self.assertTrue(len(location) == 0)
 
+
+class TestCategoryModel(TestCase):
+    def setUp(self):
+        self.category = Category(name= "Memes")
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_by_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)> 0 )
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
