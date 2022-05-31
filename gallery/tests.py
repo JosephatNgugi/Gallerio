83805c58979ebf95958c63502e299bb2bda1bf4d
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-31 06:38:08 +0300

    create tests for image model class

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class TestImageModel(TestCase):
+    def setUp(self):
+        self.location = Location(name="Nairobi")
+        self.location.save_location()
+
+        self.category = Category(name="travel")
+        self.category.save_category()
+
+        self.image_test =Image(id=1,name='image',description='this is a test image',location=self.location,category=self.category)
+        self.image_test.save_image()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test,Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        retrieve = Image.objects.all()
+        self.assertTrue(len(retrieve)>0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        images =Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+    def test_update_image(self):
+        self.image_test.save_image()
+        self.image_test.update_image(self.image_test.id,'images/test.jpeg')
+        updated_image=Image.objects.filter(image='images/test.jpeg')
+        self.assertTrue(len(updated_image) >0)
+
+    def test_get_image_by_id(self):
+        found_image = self.image_test.get_image_by_id(self.image_test.id)
+        image = Image.objects.filter(id=self.image_test.id)
+        self.assertTrue(found_image,image)
+
+    def test_search_image_by_location(self):
+        self.image_test.save_image()
+        found_images = self.image_test.filter_by_location(location_id='1')
+        self.assertFalse(len(found_images) >= 1 )
+
+    def test_search_image_by_category(self):
+        name='travel'
+        found_image = self.image_test.search_by_name(name)
+        self.assertTrue(len(found_image)>= 0)
+
+    def tearDown(self):
+        Image.objects.all().delete()
+        Location.objects.all().delete()
+        Category.objects.all().delete()
+
