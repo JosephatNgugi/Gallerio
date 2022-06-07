d01b48b0d503c39b4712e71cf5dc71a257a5f404
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-07 14:39:33 +0300

    switch to decouple to fix cloudinary improperly configured in heroku

--- gallerio/settings.py
+++ gallerio/settings.py
@@ -17,6 +17,7 @@ import cloudinary
 import cloudinary.api_client
 import cloudinary.uploader
 from dotenv import load_dotenv, find_dotenv
+from decouple import config
 # from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,10 +29,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv(find_dotenv())
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['serene-sierra-10808.herokuapp.com']
 
@@ -89,14 +90,14 @@ WSGI_APPLICATION = 'gallerio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-if os.environ.get('MODE')=='dev':
+if config('MODE')=='dev':
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
             'PORT': '',
         }
 }
@@ -104,7 +105,7 @@ if os.environ.get('MODE')=='dev':
 else:
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL')
+            default=config('DATABASE_URL')
         )
     }
     
@@ -159,9 +160,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Cloudinary Configurations
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 cloudinary.config(
-    cloud_name = os.environ('CLOUDINARY_CLOUD_NAME'),
-    api_key = os.environ('CLOUDINARY_API_KEY'),
-    api_secret = os.environ('CLOUDINARY_SECRET'),
+    cloud_name = config('CLOUDINARY_CLOUD_NAME'),
+    api_key = config('CLOUDINARY_API_KEY'),
+    api_secret = config('CLOUDINARY_SECRET'),
     secure = True
 )
 
